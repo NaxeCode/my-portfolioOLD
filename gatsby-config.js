f1151c34829f4d1ca8119ebc0ec76277a23bcc2f
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-21 19:23:59 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
