ed9a23af5d2c73cf0244e160d3ba1fc852ed0d90
Aladdin Lee <Lucas_Naruto_AE@Yahoo.com>
2021-02-21 19:52:43 -0800

    Created React Header component

---
+++ src/pages/Header.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+export default function Header(props) {
+	return (
+		<header
+			sx={{
+				// this uses the value from `theme.space[4]`
+				padding: 4,
+				// these use values from `theme.colors`
+				color: 'background',
+				backgroundColor: 'primary',
+			}}
+		>
+			{props.children}
+		</header>
+	)
+}
